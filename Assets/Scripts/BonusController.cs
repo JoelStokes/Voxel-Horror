769b6337aa2bfe6f0442af043032715a442c5eeb
jstokesVMASC <jestokes@odu.edu>
2021-06-04 17:14:49 -0400

    Bonus Scene Setup

---
+++ Assets/Scripts/BonusController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Handles timing for Text Changes & Animation Starts
+
+public class BonusController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
